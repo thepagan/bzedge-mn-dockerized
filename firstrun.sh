3e59a980556238f29eb909977fa2d80e7f8c3364
Robert Nelson <rnelson@iLoki.local>
2021-05-08 09:25:35 -0300

    Updates to firstrun.sh to create system service

--- firstrun.sh
+++ firstrun.sh
@@ -18,3 +18,30 @@ read TXID
 echo "Output Index:"
 read OIND
 echo "mn01 $IP:1990 $MNKEY $TXID $OIND" >> ~/.bzedge/masternode.conf
+sudo touch /etc/systemd/system/bzedged.service
+sudo chown bzedge:bzedge /etc/systemd/system/bzedged.service
+ls -al /etc/systemd/system
+cat << EOF > /etc/systemd/system/bzedged.service
+[Unit]
+Description=BZEdge service
+After=network.target
+[Service]
+Type=forking
+User=bzedge
+Group=bzedge
+WorkingDirectory=/home/bzedge/.bzedge
+ExecStart=/home/bzedge/bzedged/src/bzedged -datadir=/home/bzedge/.bzedge/ - conf=/home/bzedge/.bzedge/bzedge.conf -daemon
+ExecStop=/home/bzedge/bzedge/src/bzedge-cli stop
+Restart=always
+RestartSec=3
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=10s
+StartLimitInterval=120s
+StartLimitBurst=5
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo chown root:root /etc/systemd/system/bzedged.service
+sudo systemctl daemon-reload
+sudo systemctl enable bzedged.service > /dev/null 2>&1
